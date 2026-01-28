b4dbe20d755b097b3b5aabe90240efbd4fa81dbf
alokjha099 <115351017+alokjha099@users.noreply.github.com>
2026-01-28 10:45:56 +0530

    Update config.sh

--- terraform/modules/asg/config.sh
+++ terraform/modules/asg/config.sh
@@ -1,55 +1,51 @@
 #!/bin/bash
+set -e
+
+echo "===== Initializing Java Application Server (Joget PoC) ====="
 
 
-URL='https://www.tooplate.com/zip-templates/2131_wedding_lite.zip'
-ART_NAME='2131_wedding_lite'
 TEMPDIR="/tmp/webfiles"
-
-# Set Variables for Ubuntu
-PACKAGE="apache2 wget unzip"
-SVC="apache2"
+SVC_APACHE="apache2"
+SVC_TOMCAT="tomcat9"
 
 
-echo "Running Setup on Ubuntu"
-# Installing Dependencies
-echo "########################################"
-echo "Installing packages."
-echo "########################################"
-sudo apt update
-sudo apt install $PACKAGE -y > /dev/null
-echo
+apt update -y
 
-# Start & Enable Service
-echo "########################################"
-echo "Start & Enable HTTPD Service"
-echo "########################################"
-sudo systemctl start $SVC
-sudo systemctl enable $SVC
-echo
 
-# Creating Temp Directory
-echo "########################################"
-echo "Starting Artifact Deployment"
-echo "########################################"
-mkdir -p $TEMPDIR
-cd $TEMPDIR
-echo
+apt install -y apache2 wget unzip openjdk-11-jdk tomcat9
 
-wget $URL > /dev/null
-unzip $ART_NAME.zip > /dev/null
-sudo cp -r $ART_NAME/* /var/www/html/
-echo
 
-# Bounce Service
-echo "########################################"
-echo "Restarting HTTPD service"
-echo "########################################"
-systemctl restart $SVC
-echo
+systemctl enable $SVC_APACHE
+systemctl start $SVC_APACHE
 
-# Clean Up
-echo "########################################"
-echo "Removing Temporary Files"
-echo "########################################"
-rm -rf $TEMPDIR
-echo "cleanup done"
+systemctl enable $SVC_TOMCAT
+systemctl start $SVC_TOMCAT
+
+
+a2enmod proxy proxy_http
+
+cat <<EOF >/etc/apache2/sites-available/000-default.conf
+<VirtualHost *:80>
+    ProxyPreserveHost On
+    ProxyPass / http://localhost:8080/
+    ProxyPassReverse / http://localhost:8080/
+</VirtualHost>
+EOF
+
+systemctl restart apache2
+
+
+cat  <<EOF >/var/lib/tomcat9/webapps/ROOT/index.html
+<html>
+  <head><title>Joget PoC</title></head>
+  <body>
+    <h1>Joget Java Application Server</h1>
+    <p>This EC2 instance runs Tomcat with Java.</p>
+    <p>In production, the Joget WAR file would be deployed here.</p>
+  </body>
+</html>
+EOF
+
+chown -R tomcat:tomcat /var/lib/tomcat9/webapps
+
+echo "===== Joget Java Application Server Setup Complete ====="
