15c437fa5aaafc60f6de684e10b13fd1bb259ad8
alokjha099 <115351017+alokjha099@users.noreply.github.com>
2026-01-29 22:57:06 +0530

    Update main.tf

--- terraform/root/main.tf
+++ terraform/root/main.tf
@@ -77,6 +77,8 @@ module "route53" {
   source = "../modules/route53"
   alb_dns_name = module.alb.alb_dns_name
   alb_zone_id = module.alb.alb_zone_id
+  vpc_id = module.vpc.vpc_id
+  hosted_zone_name = var.hosted_zone_name
 
 }
 
